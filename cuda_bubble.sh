f4334f4fb4cc842545407b73ad98e73b7af31356
Juhana Lankinen <juhana.lankinen@aalto.fi>
2018-08-09 11:09:20 +0300

    Added sbatch (triton script) file.

---
+++ cuda_bubble.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00						## wallclock time hh:mm:ss
+#SBATCH --gres=gpu:1 --constraint='fermi|kepler|pascal'		## use K80 or P100
+#SBATCH --mail-user=juhana.lankinen@aalto.fi --mail-type=ALL
+
+module load goolfc/triton-2017a					## toolchain
+
+srun make clean
+srun --gres=gpu:1 make run
