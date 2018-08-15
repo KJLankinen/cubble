b3c47d475af7d5d053dfe4d2133308daea9f4af0
Lankinen Juhana <juhana.lankinen@aalto.fi>
2018-08-15 14:09:42 +0300

    Revert "Some changes to script."
    This reverts commit d43f6ea000376ba99de7516b60e316ef22fdd277.

--- cuda_bubble.sh
+++ cuda_bubble.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#SBATCH --mem=24G
 #SBATCH --time=01:00:00						## wallclock time hh:mm:ss
-#SBATCH --gres=gpu:1 --constraint='pascal'			## use P100
+#SBATCH --gres=gpu:1 --constraint='pascal'		## use K80 or P100
 #SBATCH --mail-user=juhana.lankinen@aalto.fi --mail-type=ALL
 
 module load goolfc/triton-2017a					## toolchain
 
 srun make clean
 srun make
-srun --gres=gpu:1 nvprof --profile-from-start off --export-profile timeline.prof bin/cubble data.json save.json
-##srun --gres=gpu:1 bin/cubble data.json save.json
+srun --gres=gpu:1 nvprof --export-profile timeline.prof bin/cubble data.json save.json
