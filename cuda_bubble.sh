9b78bdf760abc175f542ea968a930581b4e5b7bc
Juhana Lankinen <juhana.lankinen@aalto.fi>
2018-08-16 13:39:35 +0300

    Profiling related changes.

--- cuda_bubble.sh
+++ cuda_bubble.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#SBATCH --mem=4G
 #SBATCH --time=01:00:00						## wallclock time hh:mm:ss
 #SBATCH --gres=gpu:1 --constraint='pascal'		## use K80 or P100
 #SBATCH --mail-user=juhana.lankinen@aalto.fi --mail-type=ALL
@@ -9,4 +10,4 @@ module load goolfc/triton-2017a					## toolchain
 
 srun make clean
 srun make
-srun --gres=gpu:1 nvprof --profile-from-start off bin/cubble data.json save.json
+srun --gres=gpu:1 nvprof --profile-from-start off --print-gpu-trace bin/cubble data.json save.json
