7be32a97c3d4fa34574c19896356fdae7ded6159
Juhana Lankinen <juhana.lankinen@aalto.fi>
2018-09-12 16:50:37 +0300

    Changes to triton scripts for multiple runs.

--- scripts/array_run_dont_use_directly.sh
+++ scripts/array_run_dont_use_directly.sh
@@ -10,6 +10,6 @@
 module purge
 module load goolfc/triton-2017a
 
-cd $WRKDIR/cuda_bubble/multiple_runs_data/ID_$SLURM_ARRAY_ID_TASK
+cd $WRKDIR/cuda_bubble/multiple_runs_data/$SLURM_ARRAY_ID_TASK/
 cat data/input_data.json
 echo "srun --gres=gpu:1 $WRKDIR/cuda_bubble/bin/cubble input_data.json save.json"
--- scripts/multiple_runs.sh
+++ scripts/multiple_runs.sh
@@ -5,20 +5,45 @@
 #SBATCH --gres=gpu:1 --constraint='pascal'			## P100
 #SBATCH --mail-user=juhana.lankinen@aalto.fi --mail-type=ALL
 
+PROJECT_ROOT_DIR="$WRKDIR/cuda_bubble"
+MUL_RUN_DIR="$PROJECT_ROOT_DIR/multiple_runs_data"
+PARAMS_FILE="$MUL_RUN_DIR/params.txt"
+JQ_ARG_PREFIX="--argjson"
+
 module purge
 module load goolfc/triton-2017a
 
-cd $WRKDIR/cuda_bubble/
+cd $PROJECT_ROOT_DIR
 srun make clean
 srun make final
 
-PARAMS_FILE="multiple_runs_data/params.txt"
-NUM_RUNS=0
+NUM_RUNS=-1
 
-while read line
+cd $MUL_RUN_DIR
+
+while read LINE
 do
-  echo "$line"
-  NUM_RUNS += 1
+  JQ_COMMANDS=''
+  JQ_ARGS=''
+  i=0
+  for WORD in $LINE
+  do
+    if [ $((i % 2)) == 0 ]
+    then
+      JQ_ARGS="$JQ_ARGS $JQ_ARG_PREFIX $WORD"
+      JQ_COMMANDS="$JQ_COMMANDS .$WORD = \$$WORD | "
+    else
+      JQ_ARGS="$JQ_ARGS $WORD"
+    fi
+    i=$((i + 1))
+  done
+  
+  # Remove the trailing " | "
+  JQ_COMMANDS="${JQ_COMMANDS: : -3}"
+  NUM_RUNS=$((NUM_RUNS + 1))
+
+  mkdir $NUM_RUNS $NUM_RUNS/data
+  jq $JQ_ARGS '$JQ_COMMANDS' $WRKDIR/cuda_bubble/data/input_data.json > $WRKDIR/cuda_bubble/multiple_runs/$NUM_RUNS/data/input_data.json
 done < $PARAMS_FILE
 
 echo "sbatch scripts/array_run_dont_use_directly.sh --mem=1G --time=04:00:00 --gres=gpu:1 --constraint=pascal --array=0-$NUM_RUNS"
