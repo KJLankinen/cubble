e5a9dde80c3c76ef4b19fd2c9738b5ed6a59dc65
Juhana Lankinen <juhana.lankinen@aalto.fi>
2018-09-12 14:51:45 +0300

    Changes to triton scripts & file/folder for multiple runs output.

--- scripts/array_run_dont_use_directly.sh
+++ scripts/array_run_dont_use_directly.sh
@@ -7,19 +7,9 @@
 # ====================================================================================================
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-#SBATCH --mem=1G
-#SBATCH --time=04:00:00						## wallclock time hh::mm::ss
-#SBATCH --gres=gpu:1 --constraint='pascal'			## P100
-#SBATCH --mail-user=juhana.lankinen@aalto.fi --mail-type=ALL
-#SBATCH --array=0-2
-
 module purge
 module load goolfc/triton-2017a
 
-RUN_DIR=run_$SLURM_ARRAY_TASK_ID
-
-cd $WRKDIR/cuda_bubble/
-mkdir $RUN_DIR $RUN_DIR/data
-cp $WRKDIR/cuda_bubble/data/input_data.json $RUN_DIR/data/.
-cd $RUN_DIR
+cd $WRKDIR/cuda_bubble/multiple_runs_data/ID_$SLURM_ARRAY_ID_TASK
+cat data/input_data.json
 echo "srun --gres=gpu:1 $WRKDIR/cuda_bubble/bin/cubble input_data.json save.json"
--- scripts/multiple_runs.sh
+++ scripts/multiple_runs.sh
@@ -11,4 +11,14 @@ module load goolfc/triton-2017a
 cd $WRKDIR/cuda_bubble/
 srun make clean
 srun make final
-sbatch scripts/array_run_dont_use_directly.sh
+
+PARAMS_FILE="multiple_runs_data/params.txt"
+NUM_RUNS=0
+
+while read line
+do
+  echo "$line"
+  NUM_RUNS += 1
+done < $PARAMS_FILE
+
+echo "sbatch scripts/array_run_dont_use_directly.sh --mem=1G --time=04:00:00 --gres=gpu:1 --constraint=pascal --array=0-$NUM_RUNS"
