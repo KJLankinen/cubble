777bc40368aaec575c9bde93b13965cff2c21f6f
Lankinen Juhana <juhana.lankinen@aalto.fi>
2018-10-10 17:24:58 +0300

    Revert "Removed sorting from the end if neighbor search."
    This reverts commit 4ee4b7c7d99113a68d228b5c1b6aa4e1531e7371.

--- src/Simulator.cu
+++ src/Simulator.cu
@@ -538,7 +538,7 @@ void Simulator::updateCellsAndNeighbors()
         CUDA_CALL(cudaStreamWaitEvent(neighborStreamVec[i], asyncCopyDHEvent, 0));
         cudaLaunch(findPolicy, neighborSearch,
                    i, numBubbles, numCells, static_cast<int>(pairs.getWidth()),
-                   offsets, sizes, pairs.getRowPtr(0), pairs.getRowPtr(1), r,
+                   offsets, sizes, pairs.getRowPtr(2), pairs.getRowPtr(3), r,
                    interval.x, PBC_X == 1, x,
                    interval.y, PBC_Y == 1, y
 #if (NUM_DIM == 3)
@@ -550,6 +550,15 @@ void Simulator::updateCellsAndNeighbors()
         CUDA_CALL(cudaEventRecord(neighborEventVec[i], neighborStreamVec[i]));
         CUDA_CALL(cudaStreamWaitEvent(0, neighborEventVec[i], 0));
     }
+
+    CUDA_CALL(cudaMemcpy(static_cast<void *>(pinnedInt.get()), np, sizeof(int), cudaMemcpyDeviceToHost));
+    int numPairs = pinnedInt.get()[0];
+    cubWrapper->sortPairs<int, int>(&cub::DeviceRadixSort::SortPairs,
+                                    const_cast<const int *>(pairs.getRowPtr(2)),
+                                    pairs.getRowPtr(0),
+                                    const_cast<const int *>(pairs.getRowPtr(3)),
+                                    pairs.getRowPtr(1),
+                                    numPairs);
 }
 
 void Simulator::updateData()
