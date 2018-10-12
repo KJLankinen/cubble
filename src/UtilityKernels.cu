227117e951c35bff9b8292fd857330057b899eca
Lankinen Juhana <juhana.lankinen@aalto.fi>
2018-10-12 17:01:19 +0300

    Slight change to how wrapped distance is calculated.

--- src/UtilityKernels.cu
+++ src/UtilityKernels.cu
@@ -30,14 +30,11 @@ __device__ void setFlagIfGreaterThanConstant(int idx, int *flags, double *values
 
 __device__ double getWrappedDistance(double x1, double x2, double maxDistance, bool shouldWrap)
 {
-    double distance = x1 - x2;
-    if (shouldWrap)
-    {
-        x2 = distance < -0.5 * maxDistance ? x2 - maxDistance : (distance > 0.5 * maxDistance ? x2 + maxDistance : x2);
-        distance = x1 - x2;
-    }
+    const double distance1 = x1 - x2;
+    x2 = distance < -0.5 * maxDistance ? x2 - maxDistance : (distance > 0.5 * maxDistance ? x2 + maxDistance : x2);
+    const double distance2 = x1 - x2;
 
-    return distance;
+    return shouldWrap ? distance2 : distance1;
 }
 
 __device__ double getDistanceSquared(int idx1, int idx2, double maxDistance, bool shouldWrap, double *x)
