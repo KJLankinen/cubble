299f6ca7bc6f9fa86988b52b2e8b6f4e65f94b44
Juhana Lankinen <juhana.lankinen@aalto.fi>
2019-01-23 14:37:56 +0200

    Change single quotes to double quotes for sbatch constraint.

--- scripts/array_job.py
+++ scripts/array_job.py
@@ -26,7 +26,7 @@ def main():
 #SBATCH --mem=100M\n\
 #SBATCH --time=00:10:00\n\
 #SBATCH --gres=gpu:1\n\
-#SBATCH --constraint='pascal|volta'\n\
+#SBATCH --constraint="pascal|volta"\n\
 #SBATCH --mail-user=juhana.lankinen@aalto.fi\n\
 #SBATCH --mail-type=ALL\n\
 module purge\n\
@@ -89,7 +89,7 @@ cp /tmp/$SLURM_JOB_ID/cubble " + data_dir + "\n\
 #SBATCH --mem=1G\n\
 #SBATCH --time=24:00:00\n\
 #SBATCH --gres=gpu:1\n\
-#SBATCH --constraint='pascal|volta'\n\
+#SBATCH --constraint="pascal|volta"\n\
 #SBATCH --mail-user=juhana.lankinen@aalto.fi\n\
 #SBATCH --mail-type=ALL\n\
 #SBATCH --dependency=aftercorr:" + compile_slurm_id + "\n\
