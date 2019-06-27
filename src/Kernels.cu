f024c55918e6d783f565268fa8d707eec609122c
Lankinen Juhana <juhana.lankinen@aalto.fi>
2019-06-27 10:56:20 +0300

    Fixed a bug that was introduced, when fixing a bug...

--- src/Kernels.cu
+++ src/Kernels.cu
@@ -457,11 +457,11 @@ __global__ void flowVelocityKernel(int numValues, int *numNeighbors, double *vel
 #endif
     inside = !!inside;
 
-    const double multiplier = (numNeighbors[i] > 0 ? (double)inside / numNeighbors[i] : 0.0);
-    velX[i] += multiplier * nVelX[i] + flowVel.x;
-    velY[i] += multiplier * nVelY[i] + flowVel.y;
+    const double multiplier = (numNeighbors[i] > 0 ? 1.0 / numNeighbors[i] : 0.0);
+    velX[i] += multiplier * nVelX[i] + flowVel.x * inside;
+    velY[i] += multiplier * nVelY[i] + flowVel.y * inside;
 #if (NUM_DIM == 3)
-    velZ[i] += multiplier * nVelZ[i] + flowVel.z;
+    velZ[i] += multiplier * nVelZ[i] + flowVel.z * inside;
 #endif
   }
 }
