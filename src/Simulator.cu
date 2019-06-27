c13c7460cc85472427c4f8ec91758d801f24dc57
Juhana Lankinen <juhana.lankinen@aalto.fi>
2019-06-27 22:59:27 +0300

    Increased the amount of memory reserved for neighbors in 3D case.

--- src/Simulator.cu
+++ src/Simulator.cu
@@ -1052,7 +1052,7 @@ void Simulator::reserveMemory()
     ddps[i] = deviceDoubles + i * dataStride;
 
   // Integers
-  const uint32_t avgNumNeighbors = 4 * NUM_DIM;
+  const uint32_t avgNumNeighbors = 1 << (NUM_DIM + 1);
   pairStride                     = avgNumNeighbors * dataStride;
 
   memReqI =
