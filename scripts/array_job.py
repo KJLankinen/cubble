cb09f6f4ee5ec5d25a0c5323a3ae86278110285c
Lankinen Juhana <juhana.lankinen@aalto.fi>
2019-07-11 12:35:03 +0300

    Array script changes

--- scripts/array_job.py
+++ scripts/array_job.py
@@ -33,7 +33,6 @@ def main():
     executable_path = os.path.join(data_dir, "cubble")
     array_data_dir = os.path.join(data_dir, "run_$SLURM_ARRAY_TASK_ID")
     array_input_path = os.path.join(array_data_dir, os.path.split(default_input_file)[1])
-    binary_input_path = os.path.join(array_data_dir, binary_name)
     result_file_path = os.path.join(array_data_dir, "results.dat")
     sb_modules = "cuda/10.0.130 gcc/6.3.0"
     sb_mem = "32G"
@@ -46,6 +45,7 @@ def main():
     binary_name = "state.bin"
     array_temp_dir = "/tmp/$SLURM_JOB_ID"
     continue_script_name = "continue_script.sh"
+    binary_input_path = os.path.join(array_data_dir, binary_name)
 
     compile_script = "\
 #!/bin/bash\n\
@@ -61,7 +61,7 @@ mkdir /tmp/$SLURM_JOB_ID\n\
 srun make -C " + make_dir + " BIN_PATH=/tmp/$SLURM_JOB_ID\n\
 cp /tmp/$SLURM_JOB_ID/cubble " + data_dir + "\n\
 "
-'''
+
     if not os.path.isdir(root_dir):
         print("Root dir \"" + root_dir + "\" is not a directory.")
         return 1
@@ -84,34 +84,33 @@ cp /tmp/$SLURM_JOB_ID/cubble " + data_dir + "\n\
     print("Using " + array_param_file + " as the file to modify the default input file with.")
     print("Using " + data_dir + " as the data directory for this simulation run.")
 
-    print("Launching process for compiling the binary.")
-    compile_process = subprocess.Popen(["sbatch"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    compile_stdout = compile_process.communicate(input=compile_script)[0]
-    compile_slurm_id = str(compile_stdout).strip().split(" ")[-1]
+    #print("Launching process for compiling the binary.")
+    #compile_process = subprocess.Popen(["sbatch"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    #compile_stdout = compile_process.communicate(input=compile_script)[0]
+    compile_slurm_id = 1#str(compile_stdout).strip().split(" ")[-1]
 
-    if compile_process.returncode != 0:
-        print("Compile process submission was not successful!")
-        return compile_process.returncode
+    #if compile_process.returncode != 0:
+    #    print("Compile process submission was not successful!")
+    #    return compile_process.returncode
 
-    print("Reading default input arguments.")
-    with open(default_input_file) as json_file_handle:
-        json_data = json.load(json_file_handle)
+    #print("Reading default input arguments.")
+    #with open(default_input_file) as json_file_handle:
+    #    json_data = json.load(json_file_handle)
 
-    num_runs = 0
+    #num_runs = 0
 
-    print("Creating directories and input files.")
-    with open(array_param_file) as parameter_file_handle:
-        for counter, line in enumerate(parameter_file_handle):
-            run_dir = os.path.join(data_dir, "run_" + str(counter))
-            outfile_path = os.path.join(run_dir, os.path.split(default_input_file)[1])
+    #print("Creating directories and input files.")
+    #with open(array_param_file) as parameter_file_handle:
+    #    for counter, line in enumerate(parameter_file_handle):
+    #        run_dir = os.path.join(data_dir, "run_" + str(counter))
+    #        outfile_path = os.path.join(run_dir, os.path.split(default_input_file)[1])
 
-            create_folder_and_data_file(run_dir,
-                outfile_path,
-                copy.deepcopy(json_data),
-                json.loads(line.strip()))
+    #        create_folder_and_data_file(run_dir,
+    #            outfile_path,
+    #            copy.deepcopy(json_data),
+    #            json.loads(line.strip()))
 
-            num_runs = counter
-    '''
+    #        num_runs = counter
 
     continue_script = "\
 #!/bin/bash\n\
@@ -133,7 +132,6 @@ mv -f " + array_temp_dir + "/* " + array_data_dir + "\n\
 cd " + array_data_dir + "\n\
 if [ -f " + binary_name + " ] && [ -f " + continue_script_name + " ]; then sbatch " + continue_script_name + "; fi\n\
 "
-    compile_slurm_id = 0
     array_script = "\
 #!/bin/bash\n\
 #SBATCH --job-name=cubble\n\
@@ -160,7 +158,7 @@ if [ -f " + continue_script_name + " ]; then sbatch " + continue_script_name + "
     print(array_script)
 
     return 0
-    '''
+
     print("Launching an array of processes that run the simulation.")
     array_process = subprocess.Popen(["sbatch"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     array_stdout = array_process.communicate(input=array_script)[0]
@@ -175,7 +173,6 @@ if [ -f " + continue_script_name + " ]; then sbatch " + continue_script_name + "
     print("Slurm queue of the current user:")
     print(squeue_process.communicate()[0])
     print("\nJob submission done!")
-    '''
 
 if __name__ == "__main__":
     main()
