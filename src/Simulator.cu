a5e7e9bee62a35c2deefd0847f1c7fa0eff8cc2f
Juhana Lankinen <juhana.lankinen@aalto.fi>
2019-08-21 04:12:33 +0300

    Flags are set at bubble removal

--- src/Simulator.cu
+++ src/Simulator.cu
@@ -521,6 +521,10 @@ void deleteSmallBubbles(Params &params, int numBubblesAboveMinRad)
 
   CUDA_CALL(cudaMemset(static_cast<void *>(dVolMul), 0, sizeof(double)));
 
+  KERNEL_LAUNCH(setFlagIfGreaterThanConstantKernel, params.defaultKernelSize, 0,
+                0, params.state.numBubbles, params.dips[(uint32_t)DIP::FLAGS],
+                params.ddps[(uint32_t)DDP::RP], params.inputs.minRad);
+
   KERNEL_LAUNCH(calculateRedistributedGasVolume, params.defaultKernelSize, 0, 0,
                 params.ddps[(uint32_t)DDP::TEMP1],
                 params.ddps[(uint32_t)DDP::R],
